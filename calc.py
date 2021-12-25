92c78fef7ce56dae276a32073348ae5450fa3056
Minhaj <92469140+KEAGTORB@users.noreply.github.com>
2021-12-25 14:53:08 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,11 @@
+print("""
+  ____    _    _     ____
+ / ___|  / \  | |   / ___|
+| |     / _ \ | |  | |
+| |___ / ___ \| |__| |___
+ \____/_/   \_\_____\____|
+                     [[ github KEAGTORB ]]
+    """)
+
+x= input('Write a math problem: ')
+print('Your Answer is :',eval(x))
